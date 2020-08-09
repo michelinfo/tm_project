d27d8274684ce50ac263e4b6877fbe3b7b81cb35
michelinfo <michelinforh@gmail.com>
2020-08-09 18:04:58 -0300

    project_tm

---
+++ index.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initia-scale=1, shrink-to-fit=no">
+	<meta name="description" content="Tire suas dúvidas e descubra quais são os testes da COVID-19 disponíveis para você. 07/08/2020. A melhor forma de saber se você foi infectado pelo novo ...">
+	<meta name="author" content="Michel Jesus">
+	<meta name="COVID-19, vacina, laboratório de vacina, corona vírus,">
+	<title>tm_home</title>
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css">
+
+<link rel="stylesheet" href="css/index.css">
+
+</head>
+<body id="page-top">
+
+<nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #132e74; padding:0px ">
+
+	<div class="container"> 
+	
+		<form class="form-inline">
+
+ 			<button class="btn btn-info button-top text-uppercase" type="button"><a class="nav-link 
+ 			js-scroll-trigger btn-agenda-top" href="#agenda_exames">agende seu exame</a>
+ 			</button>
+    
+		</form>
+
+	</div>
+
+</nav>
+
+<nav class="navbar navbar-expand-lg navbar-light">
+
+  	<div class="container h-100"> 	
+
+		<a class="navbar-brand js-scroll-trigger" href="#page-top">
+			<img src="img/logo.png" class="img-logo">
+		</a>
+
+		<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" 
+		data-target="#navbarResponsive" aria-expanded="false" aria-label="Toggle Navigation">
+		Menu <i class="fas fa-bars"></i>
+		</button>
+		
+		<div class="collapse navbar-collapse" id="navbarResponsive">
+		
+			<ul class="navbar-nav text-uppercase ml-auto menu-text">
+				
+				<li class="nav-item">
+					<a class="nav-link js-scroll-trigger" href="#faleconosco">Fale Conosco</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link js-scroll-trigger" href="#ciencia">Ciência</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link js-scroll-trigger" href="#agenda">Agenda</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link js-scroll-trigger" href="#noticias">Notícias</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link js-scroll-trigger" href="#educação">Educação</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link js-scroll-trigger" href="#cadastro">Cadastro</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link js-scroll-trigger" href="#sobrenos">Sobre Nós</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link js-scroll-trigger" href="#produtos">Produtos</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link js-scroll-trigger" href="#blog"></a>
+				</li>
+				
+			</ul>
+				
+		</div>
+
+	</div>
+
+</nav>
+
+<!-- CABEÇALHO -->
+
+<header class="carousel-top">
+
+	<div class="container">
+
+		<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  			<ol class="carousel-indicators">
+  			<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    		<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    		<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  			</ol>
+  			<div class="carousel-inner">
+    			<div class="carousel-item active">
+    			<img class="d-block w-100" src="img/home1.png" alt="First slide">
+    			</div>
+   					<div class="carousel-item">
+    			  	<img class="d-block w-100 img-02" src="img/home2.png" alt="Second slide">
+   					</div>
+   						<div class="carousel-item">
+  						<img class="d-block w-100 img-03" src="img/home3.png" alt="Third slide">
+    					</div>
+  			</div>
+  				<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+  					<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    				<span class="sr-only">Previous</span>
+  				</a>
+  				<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+  					<span class="carousel-control-next-icon" aria-hidden="true"></span>
+    				<span class="sr-only">Next</span>
+  				</a>
+		</div>
+
+	</div>
+
+
+
+</header>
+
+<header class="cabecalho">
+	<div class="container">
+		<img class="d-block w-100 image-secun" src="img/quemsomos2.png" alt="Third slide">
+		<div class="intro-text1">
+					Agende seu
+		</div>
+			<div class="intro-head">
+					Teste RT-PCR
+			</div>
+				<div class="intro-text2">
+					Padrão Ouro, para o combate ao COVID-19.
+				</div>
+				<button type="button" class="btn btn-primary btn-lg bt-secun">AGENDE SEU EXAME</button>
+			</div>
+	</div>
+</header>
+
+<footer class="footer navbar-fixed-bottom">
+
+	<nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #132e74; padding:0px ">
+
+	<div class="container"> 
+	
+		<form class="form-inline">
+
+ 			<button class="btn btn-info button-top text-uppercase" type="button"><a class="nav-link 
+ 			js-scroll-trigger btn-agenda-top" href="#agenda_exames">agende seu exame</a>
+ 			</button>
+    
+		</form>
+
+	</div>
+
+</nav>
+</footer>
+</body>
+</html>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
---
+++ login.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css">
+
+<link rel="stylesheet" href="css/styles-login.css">
+
+</head>
+<body>
+
+	<div class="container h-100">
+
+		<div class="d-flex justify-content-center h-100">
+
+			<div class="card_login">
+
+				<div class="d-flex justify-content-center">
+
+					<div class="area_logo">
+
+						<a href="index.php"><img src="img/logon.png" class="logo_circular"></a>
+
+					</div>
+
+				</div>
+
+				<div class="d-flex justify-content-center form_login">
+
+					<form>
+						<div class="input-group mb-3">
+
+							<div class="input-group-append">
+
+								<spam class="input-group-text">
+
+								<i class="fas fa-user"></i>
+
+								</spam>
+
+							</div>
+
+							<input type="text" name="" class="form-control input_usuario" placeholder="Usuário">
+
+						</div>
+
+						<div class="input-group mb-3">
+
+							<div class="input-group-append">
+
+								<spam class="input-group-text">
+
+								<i class="fas fa-key"></i>
+
+								</spam>
+
+							</div>
+
+							<input type="text" name="" class="form-control input_usuario" placeholder="Senha">
+
+						</div>
+
+						<div class="input-group">
+
+							<div class="custom-control custom-checkbox">
+
+								<input type="checkbox" name="" class="custom-control-input" id="customControlInline">
+
+								<label class="custom-control-label lembrar_login" for="customControlInline">Lembrar-me</label> 
+
+							</div>
+
+						</div>
+
+					</form>
+
+				</div>
+
+				<div class="d-flex justify-content-center mt-3 area_botao">
+
+					<button type="button" name="button" class= "btn btn_login">
+
+						Login
+
+					</button>
+						
+				</div>
+
+				<div class="mt-4 area_links">
+
+					<div class="d-flex justify-content-center links">
+
+						Não possui cadastro? <a href="#" class="ml-2 cad_login">
+						Cadastre-se </a>
+
+					</div>
+
+					<div class="d-flex justify-content-center links">
+					
+						<a href="#" class="ml-2 rec_login">Recuperar Senha</a>
+					
+					</div>
+
+				</div>
+
+			</div>
+
+		</div>
+		
+	</div>
+
+</body>
+</html>
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
